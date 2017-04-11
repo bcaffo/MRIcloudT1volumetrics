d0ca81a1601e7a0c8a58f15f716bd794a3aa53a1
Brian Caffo <bcaffo@gmail.com>
2017-04-11 15:21:18 -0400

    Fixed an errir in Utils

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,7 @@ addSubjectICV = function(subjectData){
     require(dplyr)
     l1t1 = filter(subjectData, level == 1, type == 1)
     icv = sum(l1t1$volume)
-    subjectData = mutate(subjectData, volume = icv)
+    subjectData = mutate(subjectData, icv = icv)
     return(subjectData)
 }
 
