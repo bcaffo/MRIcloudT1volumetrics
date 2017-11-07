f61263ed8d69446f581c14518d372bd5ee3fb6c1
Brian Caffo <bcaffo@users.noreply.github.com>
2017-11-06 22:05:29 -0500

    Added an id tracker

--- R/readFileList.R
+++ R/readFileList.R
@@ -3,15 +3,21 @@
 #' @param fileList a textlist of files 
 #' @param levelList an optional list of levels to include, needs equal length as \code{typeList}
 #' @param typeList an optional list of types to include, needs equal length to \code{levelList}
+#' @param idList an optional list of ids, otherwise is 1 : length(fileList)
 #' @description A function for reading in a collection of files of T1 volumetric output
 #' from MRIcloud. The files must be a list of files
 #' @importFrom magrittr "%>%"
 #' @importFrom dplyr filter mutate
 #' @export
-readFileList = function(fileList, levelList = 5, typeList = 1){
+readFileList = function(fileList, levelList = 5, typeList = 1, idList = NULL){
     if (length(levelList) != length(typeList)) {
         stop("levelList and typeList must have equal length")
     }
+    if (is.null(idList)) idList = 1 : length(fileList)
+    else if (length(idList) != length(fileList)) {
+        stop("idList and fileList must have equal length")
+    }
+   
     level = type = NULL
     rm(list = c("level", "type"))    
     rval = NULL
@@ -20,7 +26,7 @@ readFileList = function(fileList, levelList = 5, typeList = 1){
         tempDat = readSubject(f) %>%
             subject2df() %>% 
             filter(level %in% levelList, type %in% typeList) %>%
-            mutate(id = i)
+            mutate(id = idList[i])
         rval = rbind(rval, tempDat)
     }
     rval
