cedd781f0c4f93a34cc13b031b6d3f624968bf82
Brian Caffo <bcaffo@gmail.com>
2018-08-01 14:32:55 +0000

    added gui file and doc

---
+++ R/runGUI.R
@@ -0,0 +1,31 @@
+#' @name rungui
+#' @title A function for running a gui that works with MRICloud output
+#' @param example name of gui, currently only \code{'loaddata'}
+#' @description A function that launches a gui for the loading and analysis of MRICloud output
+#' currently there's only one app named \code{'loaddata'}
+#' @importFrom tidyverse
+#' @export
+## Adapted from Dean Attali's code
+runGUI = function(example) {
+    # locate all the shiny app examples that exist
+    validExamples = list.files(system.file("shinyApps", package = "MRIcloudT1volumetrics"))
+    
+    validExamplesMsg =
+        paste0(
+            "Valid examples are: '",
+            paste(validExamples, collapse = "', '"),
+            "'")
+    
+    # if an invalid example is given, throw an error
+    if (missing(example) || !nzchar(example) ||
+        !example %in% validExamples) {
+        stop(
+            'Please run `runExample()` with a valid example app as an argument.\n',
+            validExamplesMsg,
+            call. = FALSE)
+    }
+    
+    # find and launch the app
+    appDir = system.file("shinyApps", example, package = "MRIcloudT1volumetrics")
+    shiny::runApp(appDir, display.mode = "normal")
+}
