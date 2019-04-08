06e04e492b1bb2c57a2e079a6449da8d0f4dbf0e
Brian Caffo <bcaffo@gmail.com>
2019-04-08 17:17:00 +0000

    Added some doc files

--- R/mergeHierarchy.R
+++ R/mergeHierarchy.R
@@ -2,3 +2,4 @@
 #' @title Merges the regional hierarchy to MRICloud data frame
 #' @param mricData the output from readSubject or readFileList
 #' @description This function simply merges the hierarchical data 
+#' 
