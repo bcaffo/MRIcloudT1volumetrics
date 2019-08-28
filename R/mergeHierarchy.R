4677f5329d9877839dc016d0f16580e86e7bce10
Brian Caffo <bcaffo@users.noreply.github.com>
2019-08-28 11:25:29 -0400

    submit_package_release

--- R/mergeHierarchy.R
+++ R/mergeHierarchy.R
@@ -1,5 +1,5 @@
 #' @name mergeHierarchy
-#' @title Merges the regional hierarchy to MRICloud data frame
-#' @param mricData the output from readSubject or readFileList
-#' @description This function simply merges the hierarchical data 
+#' @title Merges the regional hierarchy to MRICloud data frame.
+#' @param mricData the output from readSubject or readFileList.
+#' @description This function simply merges the hierarchical data.
 #' 
